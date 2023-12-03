454b81d7e85ec5fd51d0cd251bbec803ae5112bd
Rich Churcher <rich.churcher@gmail.com>
2023-12-03 13:59:53 +1300

    Curious

--- cloud_game/src/main.rs
+++ cloud_game/src/main.rs
@@ -2,7 +2,19 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "a cloud of hexlings".to_string(),
+                // Bind to canvas included in `index.html`
+                canvas: Some("#bevy".to_owned()),
+                // The canvas size is constrained in index.html and build/web/styles.css
+                fit_canvas_to_parent: true,
+                // Tells wasm not to override default event handling, like F5 and Ctrl+R
+                prevent_default_event_handling: false,
+                ..default()
+            }),
+            ..default()
+        }))
         .add_state::<cloud_lib::GameState>()
         .add_systems(Startup, setup)
         .add_plugins(cloud_lib::menu::MenuPlugin)
